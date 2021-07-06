c1c70d673e19822e439f3a53d4106f484b9e9ed0
jaredg-ai <jaredward.harod@gmail.com>
2021-07-06 10:40:01 -0600

    checked for spelling

--- budget/ExpenseCategories.py
+++ budget/ExpenseCategories.py
@@ -18,9 +18,9 @@ def main():
     print(timeit.timeit(stmt='expenses.categorize_for_loop()',
     setup =
     '''
-    from . import Expense
-    expenses = Espense.Expenses()
-    expenses.read_expenses('data/spending_data.csv')
+from . import Expense
+expenses = Expense.Expenses()
+expenses.read_expenses('data/spending_data.csv')
     ''',
     number=100000,
     globals=globals()
@@ -29,15 +29,15 @@ def main():
     print(timeit.timeit(stmt='expenses.categorize_set_comprehension()',
     setup =
     '''
-    from . import Expense
-    expenses = Espense.Expenses()
-    expenses.read_expenses('data/spending_data.csv')
+from . import Expense
+expenses = Expense.Expenses()
+expenses.read_expenses('data/spending_data.csv')
     ''',
     number=100000,
     globals=globals()
     ))
 
-    fix, ax = plt.subplots()
+    fig, ax = plt.subplots()
     labels = ['Necessary', 'Food', 'Unnecessary']
     divided_expenses_sum = []
     for category_exps in divided_set_comp:
